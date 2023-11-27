6cd45d09b751038edaaa744157de56947dac43b1
Sam Feldman <feldmans@anl.gov>
2023-11-27 14:42:15 -0600

    yes

---
+++ PrintStatements/LanguageFacts.java
@@ -0,0 +1,14 @@
+//using semicolons ;
+public class LanguageFacts {
+    public static void main(String[] args) {
+      // Press enter or return on your keyboard after each semicolon!
+      
+      System.out.println("Java is a class-based language.");
+      System.out.println("Java classes have a 'main' method.");
+      System.out.println("Java statements end with a semicolon.");
+  
+      System.out.println("Programming is fun");
+      //Remember to place a semicolon at the end of the statement!
+    }
+  }
+  
