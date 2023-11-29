f61d39ae9b78b5faca2deb5407182ce4afd6bf60
Sam Feldman <feldmans@anl.gov>
2023-11-29 16:12:42 -0600

    updates

---
+++ module2/Final.java
@@ -0,0 +1,10 @@
+package module2;
+
+public class Final {
+	public static void main(String[] args) { 
+      final double pi =3.14;
+      System.out.println(pi);
+      //pi=4.3; cannot be reassigned
+
+	}
+}
