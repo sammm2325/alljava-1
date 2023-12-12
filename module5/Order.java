543ad8440ff9b8b1ffe12337b770b6751067920e
Sam Feldman <feldmans@anl.gov>
2023-12-12 14:12:17 -0600

    updates

---
+++ module5/Order.java
@@ -0,0 +1,26 @@
+package module5;
+
+public class Order {
+  
+  public static void main(String[] args) {
+    
+    double itemCost = 30.99;
+    
+    // Write an if-then statement:
+    if(itemCost>24.00){
+      System.out.println("High value item!");
+    }
+    
+
+    boolean isFilled = false;
+    
+    // Write an if-then-else statement:
+    if(isFilled==true){
+      System.out.println("Shipping");
+    }
+    else{
+      System.out.println("Order not ready");
+    }
+  }
+  
+} 
