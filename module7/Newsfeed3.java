c510018cb3848659a1c492e61f12916ae31e6d86
Sam Feldman <feldmans@anl.gov>
2023-12-14 17:24:51 -0600

    arraylists

---
+++ module7/Newsfeed3.java
@@ -0,0 +1,34 @@
+package module7;
+
+import java.util.Arrays;
+
+public class Newsfeed3 {
+  
+  String[] topics = {"Opinion", "Tech", "Science", "Health"};
+  int[] views = {0, 0, 0, 0};
+  String[] favoriteArticles;
+  
+  public Newsfeed3(){
+    // Initialize favoriteArticles here:
+    favoriteArticles = new String[10];
+    
+  }
+  
+  public void setFavoriteArticle(int favoriteIndex, String newArticle){
+    // Add newArticle to favoriteArticles:
+    favoriteArticles[favoriteIndex]=newArticle;
+    
+    
+  }
+    
+  public static void main(String[] args){
+    Newsfeed3 sampleFeed = new Newsfeed3();
+    
+    sampleFeed.setFavoriteArticle(2, "Humans: Exterminate Or Not?");
+    sampleFeed.setFavoriteArticle(3, "Organic Eye Implants");
+    sampleFeed.setFavoriteArticle(0, "Oil News");
+    
+    System.out.println(Arrays.toString(sampleFeed.favoriteArticles));
+  }
+}
+
---
+++ module8/List.java
@@ -0,0 +1,19 @@
+package module8;
+
+import java.util.ArrayList;
+
+class List {
+  
+  public static void main(String[] args) {
+    ArrayList<String> List = new ArrayList<String>();
+    List.add("anything");
+    List.add("anything1");
+    List.size();
+    List.get(0);
+    List.set(0, "something else");
+    List.remove("something else");
+    List.indexOf("anything1");
+
+  }
+  
+}
