bc57744991ffd8d71ea7b50d9c37ac8fcf907e66
Sam Feldman <feldmans@anl.gov>
2023-12-19 14:20:28 -0600

    updates

---
+++ AccessorMutator/CheckingAccount.java
@@ -0,0 +1,16 @@
+package AccessorMutator;
+
+public class CheckingAccount{
+    private String name;
+    private int balance;
+    private String id;
+  
+    public CheckingAccount(String inputName, int inputBalance, String inputId){
+      name = inputName;
+      balance = inputBalance;
+      id = inputId;
+    }
+  
+    //Write new methods here:
+    
+  }
