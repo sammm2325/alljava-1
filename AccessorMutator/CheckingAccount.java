07ce49698ef3c97a3d53e2f8790beab28448971f
sammm2325 <113053473+sammm2325@users.noreply.github.com>
2024-01-04 15:04:42 -0600

    updates

--- AccessorMutator/CheckingAccount.java
+++ AccessorMutator/CheckingAccount.java
@@ -1,16 +1,22 @@
 package AccessorMutator;
 
 public class CheckingAccount{
-    private String name;
-    private int balance;
-    private String id;
-  
-    public CheckingAccount(String inputName, int inputBalance, String inputId){
-      name = inputName;
-      balance = inputBalance;
-      id = inputId;
-    }
-  
-    //Write new methods here:
-    
+  private String name;
+  private int balance;
+  private String id;
+
+  public CheckingAccount(String inputName, int inputBalance, String inputId){
+    name = inputName;
+    balance = inputBalance;
+    id = inputId;
   }
+
+  //Write new methods here:
+  public int getBalance(){
+    return this.balance;
+  }
+  public void setBalance(int newBalance){
+    this.balance=newBalance;
+  }
+  
+}
