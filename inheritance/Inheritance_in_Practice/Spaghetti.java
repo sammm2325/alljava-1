2723f8412a7b2f81136e030bf843688e25c0ba4f
Feldman <feldmans@anl.gov>
2024-01-11 12:45:54 -0600

    updates

--- inheritance/Inheritance_in_Practice/Spaghetti.java
+++ inheritance/Inheritance_in_Practice/Spaghetti.java
@@ -1,3 +1,8 @@
 class Spaghetti extends Noodle {
 
+    Spaghetti(double lenInCent, double wthInCent, String shp, String ingr) {
+        super(lenInCent, wthInCent, shp, ingr);
+        //TODO Auto-generated constructor stub
+    }
+    
 }
