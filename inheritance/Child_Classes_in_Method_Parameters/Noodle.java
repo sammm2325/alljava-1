ccb314ffd4911aa130b06edb983186e82908c3f4
Feldman <feldmans@anl.gov>
2024-01-11 13:32:52 -0600

    updates

---
+++ inheritance/Child_Classes_in_Method_Parameters/Noodle.java
@@ -0,0 +1,26 @@
+package inheritance.Child_Classes_in_Method_Parameters;
+
+class Noodle {
+  
+    protected double lengthInCentimeters;
+    protected double widthInCentimeters;
+    protected String shape;
+    protected String ingredients;
+    protected String texture = "brittle";
+    
+    Noodle(double lenInCent, double wthInCent, String shp, String ingr) {
+      
+      this.lengthInCentimeters = lenInCent;
+      this.widthInCentimeters = wthInCent;
+      this.shape = shp;
+      this.ingredients = ingr;
+      
+    }
+    
+    public String getCookPrep() {
+      
+      return "Boil noodle for 7 minutes and add sauce.";
+      
+    }
+    
+  }
